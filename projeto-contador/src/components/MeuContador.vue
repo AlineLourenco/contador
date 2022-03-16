196cce0914f130457106e0af0854b8766d49e1f5
AlineLourenco <alinelaurentius@gmail.com>
2022-03-16 14:42:52 -0300

    meu primeiro commit

---
+++ projeto-contador/src/components/MeuContador.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="contador">
+        <h2>Contador : {{ contador }}</h2>
+        <button @click="diminuirUm">-</button>
+        <button @click="somarUm">+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MeuContador',
+    data() {
+        return {
+            contador: 0
+        }
+    },
+    methods: {
+        diminuirUm() {
+            this.contador--;
+        },
+        somarUm() {
+            this.contador++;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h2 {
+        font-weight: bold;
+    }
+</style>
